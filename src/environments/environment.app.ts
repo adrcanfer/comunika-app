3260aa9f6893f40c2e7314b73bb8516ea56e1ae9
Adrián Cantón <adriancanton@gmail.com>
2025-10-13 20:56:47 +0200

    Configure prod environment

--- src/environments/environment.app.ts
+++ src/environments/environment.app.ts
@@ -1,8 +1,12 @@
+const production = true;
+
 export const environment = {
-  production: true,
+  production: production,
   baseFrontendUrl: 'https://www.comunikame.es',
   baseAppLinkUrl: 'https://comunikame.es',
   eventPath: 'web/event',
-  baseBackendUrl: 'https://19dlqvyf7c.execute-api.eu-west-1.amazonaws.com/dev/',
+  baseBackendUrl: production 
+    ? 'https://jsvtvde9gc.execute-api.eu-west-2.amazonaws.com/prod/' 
+    : 'https://19dlqvyf7c.execute-api.eu-west-1.amazonaws.com/dev/',
   mode: 'app'
 };
